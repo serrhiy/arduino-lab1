17ffe46e3cf7f2099dd0487cf7922ea92eb334bb
Serhii Lytvynenko <serhii.lytvynenko-ext@jci.com>
2026-09-09 00:32:30 +0300

    Create Pull Up resistor scheme example

--- src/main.cc
+++ src/main.cc
@@ -1,14 +1,16 @@
 #include <Arduino.h>
 
+constexpr uint8_t led = 12;
+constexpr uint8_t button = 2;
+
 void setup()
 {
-    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(button, INPUT);
+    pinMode(led, OUTPUT);
 }
 
 void loop()
 {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    const uint8_t button_state = digitalRead(button);
+    digitalWrite(led, button_state);
 }
