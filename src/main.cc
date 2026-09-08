2ffe8b55fc874cfe29a9df9350004106f7165e68
Serhii Lytvynenko <serhii.lytvynenko-ext@jci.com>
2026-09-09 02:00:05 +0300

    Add example with button debounce

--- src/main.cc
+++ src/main.cc
@@ -1,16 +1,66 @@
 #include <Arduino.h>
 
+class Button final
+{
+    static constexpr unsigned long DEBOUNCE_MS = 5;
+
+    uint8_t button_pin;
+    int idle_status;
+    int last_button_status;
+    unsigned long last_time_called;
+
+  public:
+    explicit Button(uint8_t button_pin, int idle_status = LOW) noexcept
+        : button_pin{button_pin}, idle_status{idle_status}, last_button_status{idle_status}, last_time_called{0}
+    {
+    }
+
+    void begin() const noexcept
+    {
+        pinMode(button_pin, INPUT);
+    }
+
+    bool isButtonPressed() noexcept
+    {
+        if (last_time_called == 0)
+        {
+            const int current_button_status = digitalRead(button_pin);
+            if (current_button_status != last_button_status)
+            {
+                last_time_called = millis();
+            }
+            return false;
+        }
+        else if (millis() - last_time_called <= DEBOUNCE_MS)
+        {
+            return false;
+        }
+        last_time_called = 0;
+        const int current_button_status = digitalRead(button_pin);
+        if (current_button_status != last_button_status)
+        {
+            last_button_status = current_button_status;
+            return current_button_status != idle_status;
+        }
+        return false;
+    }
+};
+
 constexpr uint8_t led = 12;
-constexpr uint8_t button = 2;
+Button button{2};
 
 void setup()
 {
-    pinMode(button, INPUT);
     pinMode(led, OUTPUT);
+    button.begin();
 }
 
 void loop()
 {
-    const uint8_t button_state = digitalRead(button);
-    digitalWrite(led, button_state);
+    if (button.isButtonPressed())
+    {
+        const int led_status = digitalRead(led);
+        const int target_led_status = led_status == LOW ? HIGH : LOW;
+        digitalWrite(led, target_led_status);
+    }
 }
